266bf54798c4be0d752507c4f9e81a708a51b0c1
MiloEliot02 <mtselapedi5@gmail.com>
2025-03-18 15:46:28 +0200

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,12 @@
+# A list of numbers
+my_list = [10, 20, 30, 40]
+# Insert 15 at the second position
+my_list.insert(1, 15)
+# Remove the last element
+my_list.pop()
+# Sort the list in ascending order
+my_list.sort()
+# Find and print the index of the list
+index_of_30 = my_list.index(30)
+# Print the list
+print(index_of_30)
